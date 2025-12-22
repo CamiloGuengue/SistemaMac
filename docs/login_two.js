7d3747ca46937480169194ea9d760926a4dbb8f3
CamiloGuengue <guenguec@gmail.com>
2025-12-22 17:51:00 -0500

    intento cambios despliegue

--- docs/login_two.js
+++ docs/login_two.js
@@ -21,12 +21,12 @@ const loginArrow = document.getElementById('loginArrow');
 // Agregamos el evento click
 loginArrow.addEventListener('click', () => {
     // Aquí pones la ruta del archivo principal
-    window.location.href = '/MiniOs/index.html'; // ⚡ cambia 'principal.html' por tu archivo
+    window.location.href = "MiniOs/index.html"; 
 });
 
 const backArrow = document.getElementById('backArrow');
 
 backArrow.addEventListener('click', () => {
     // Redirige a la página de selección de usuario
-    window.location.href = 'index.html'; // ⚡ Cambia según tu ruta
+    window.location.href = 'index.html';
 });
